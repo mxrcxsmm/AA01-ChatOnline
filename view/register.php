5765090116e2241e4461ab0ef604afeb68e37d2b
alvareeezzz <daviddalvarez20@gmail.com>
2024-11-03 19:58:34 +0100

    Estilos v1

--- view/register.php
+++ view/register.php
@@ -10,6 +10,7 @@ require '../bd/conexion.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 
 <body>
@@ -17,21 +18,21 @@ require '../bd/conexion.php';
         <div>
             <label>Usuario:</label>
             <input type="text" name="usuario" value="<?php echo isset($_SESSION['usuario']) ? htmlspecialchars($_SESSION['usuario']) : ''; ?>">
-            <p style="color: red;"><?php echo isset($_SESSION['usuarioError']) ? $_SESSION['usuarioError'] : ''; ?></p>
-            <p style="color: red;"><?php echo isset($_GET['usuarioVacio']) ? "El usuario no puede estar vacío." : ""; ?></p>
-            <p style="color: red;"><?php echo isset($_GET['usuarioMal']) ? "El usuario solo puede contener letras y números." : ""; ?></p>
+            <p class="error-message"><?php echo isset($_SESSION['usuarioError']) ? $_SESSION['usuarioError'] : ''; ?></p>
+            <p class="error-message"><?php echo isset($_GET['usuarioVacio']) ? "El usuario no puede estar vacío." : ""; ?></p>
+            <p class="error-message"><?php echo isset($_GET['usuarioMal']) ? "El usuario solo puede contener letras y números." : ""; ?></p>
         </div>
         <div>
             <label>Nombre:</label>
             <input type="text" name="nombre" value="<?php echo isset($_SESSION['nombre']) ? htmlspecialchars($_SESSION['nombre']) : ''; ?>">
-            <p style="color: red;"><?php echo isset($_SESSION['nombreError']) ? $_SESSION['nombreError'] : ''; ?></p>
-            <p style="color: red;"><?php echo isset($_GET['nombreVacio']) ? "El nombre no puede estar vacío." : ""; ?></p>
+            <p class="error-message"><?php echo isset($_SESSION['nombreError']) ? $_SESSION['nombreError'] : ''; ?></p>
+            <p class="error-message"><?php echo isset($_GET['nombreVacio']) ? "El nombre no puede estar vacío." : ""; ?></p>
         </div>
         <div>
             <label>Contraseña:</label>
             <input type="password" name="psswrd">
-            <p style="color: red;"><?php echo isset($_SESSION['psswrdError']) ? $_SESSION['psswrdError'] : ''; ?></p>
-            <p style="color: red;"><?php echo isset($_GET['psswrdVacio']) ? "La contraseña no puede estar vacía." : ""; ?></p>
+            <p class="error-message"><?php echo isset($_SESSION['psswrdError']) ? $_SESSION['psswrdError'] : ''; ?></p>
+            <p class="error-message"><?php echo isset($_GET['psswrdVacio']) ? "La contraseña no puede estar vacía." : ""; ?></p>
         </div>
         <input type="submit" value="Registrarse">
         <p>¿Ya tienes cuenta? <a href="login.php">Inicia sesión aquí</a>.</p>
